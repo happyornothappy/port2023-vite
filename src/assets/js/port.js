aa1b41fd72b54c645840d8113f333e453cd988af
happyornothappy <101691696+happyornothappy@users.noreply.github.com>
2023-09-01 18:58:00 +0900

    포트폴리오 완료

--- src/assets/js/port.js
+++ src/assets/js/port.js
@@ -1,3 +1,22 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 export function port(){
-    console.log(port)
+    gsap.registerPlugin(ScrollTrigger);
+
+    const horSection = gsap.utils.toArray(".port-item");
+
+    gsap.to(horSection, {
+        xPercent: -120 * (horSection.length - 1),
+        ease: "none",
+        scrollTrigger: {
+            trigger:"#port",
+            start:"top 100px",
+            end:"+=3000 ",
+            pin:true,
+            scrub:1,
+            makers: true,
+            invalidateOnRefresh:true,
+            anticipatePin:1,
+        },
+    });
 }
